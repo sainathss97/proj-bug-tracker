998c565bab7242ff722a9b7291fa6600167931f1
sainathss97 <sainath.ss97@gmail.com>
2021-06-01 11:47:14 +0530

    Update urls.py

--- task/urls.py
+++ task/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     #path('',include('bug.urls')),
     
     url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
